b48c74f1fca51a4071d15b3ce9d326f88d11aa7b
Егор Матяс <play.filin@gmail.com>
2023-03-02 14:12:04 +0300

    feat: add basic components ans pages

---
+++ src/components/CoinAddModal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CoinAddModal: React.FC = () => {
+  return <div>ModalAddCrypto</div>;
+};
+
+export default CoinAddModal;
---
+++ src/components/PortfolioModal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PortfolioModal: React.FC = () => {
+  return <div>PortfolioModal</div>;
+};
+
+export default PortfolioModal;

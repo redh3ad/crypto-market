5793858611f2cf2120465195b406b60bda1d06b2
Егор Матяс <play.filin@gmail.com>
2023-03-02 15:02:03 +0300

    feat: add react-router, lauout and routes, notfound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import CoinInfo from './pages/CoinInfo';
+import MainLayout from './layouts/MainLayout';
+import NotFound from './pages/NotFound';
 
 const App: React.FC = () => {
-  return <div className='app'>App</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<MainLayout />}>
+        <Route path='/' element={<Main />} />
+        <Route path='/assets/:id' element={<CoinInfo />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;

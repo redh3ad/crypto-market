328fed827e73c68173f39e849d683bccc4917c48
Егор Матяс <play.filin@gmail.com>
2023-03-03 16:31:24 +0300

    feat: add type for received values

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+export type TCryptoInfo = {
+  id: string;
+  rank: string;
+  symbol: string;
+  name: string;
+  supply: string;
+  maxSupply: string | null;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  priceUsd: string;
+  changePercent24Hr: string | null;
+  vwap24Hr: string;
+};

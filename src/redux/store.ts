a6d42b08b5a18b327aeb9234527bf3eeaf342678
Егор Матяс <play.filin@gmail.com>
2023-03-06 16:39:33 +0300

    fix: changed reducer name

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import crypto from './cryptoSlice';
+import cryptos from './cryptoSlice';
 
 export const store = configureStore({
   reducer: {
-    crypto,
+    cryptos,
   },
 });
 

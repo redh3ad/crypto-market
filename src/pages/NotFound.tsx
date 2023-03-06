a57c30b5fdef69804be0c5d6ac3f3a27c81e1ddf
Егор Матяс <play.filin@gmail.com>
2023-03-06 11:51:01 +0300

    feat: added notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import SvgNotFound from '../assets/404.svg';
+import SvgTitleNotFound from '../assets/404_title.svg';
+
+const SvgImage = styled.img`
+  width: calc(100vw - 65%);
+  @media (max-width: 850px) {
+    width: calc(100vw - 40%);
+  }
+  @media (max-width: 500px) {
+    width: calc(100vw - 25%);
+  }
+`;
+
+const SvgTitle = styled.img`
+  width: calc(100vw - 80%);
+  @media (max-width: 850px) {
+    width: calc(100vw - 55%);
+  }
+  @media (max-width: 500px) {
+    width: calc(100vw - 40%);
+  }
+`;
 
 const NotFound: React.FC = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className='app-not-found'>
+      <SvgImage src={SvgNotFound} alt='not found image' />
+      <SvgTitle src={SvgTitleNotFound} alt='not found image' />
+    </div>
+  );
 };
-
 export default NotFound;

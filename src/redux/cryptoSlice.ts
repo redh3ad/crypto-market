dc85f635f22525cd6ac3b9ac1010169d6da52fc0
Егор Матяс <play.filin@gmail.com>
2023-03-06 16:40:04 +0300

    feat: added async thunk for fetching all cryptos from api

--- src/redux/cryptoSlice.ts
+++ src/redux/cryptoSlice.ts
@@ -1,17 +1,43 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios, { AxiosError } from 'axios';
 import { TCryptoInfo } from '../types/types';
 
 interface ICryptoState {
   cryptos: TCryptoInfo[];
   portfolio: TCryptoInfo[];
   modalStatus: boolean;
+  loading: boolean;
+  error: string | null;
 }
 
 const initialState: ICryptoState = {
   cryptos: [],
   portfolio: [],
   modalStatus: false,
+  loading: false,
+  error: null,
 };
+
+export const fetchAllCryptos = createAsyncThunk<
+  TCryptoInfo[],
+  undefined,
+  { rejectValue: string }
+>('cryptos/fetchAllCryptos', async (_, { rejectWithValue }) => {
+  try {
+    const { data } = await axios.get<{ data: TCryptoInfo[] }>(
+      'https://api.coincap.io/v2/assets',
+    );
+    if (data.data) {
+      console.log(data.data);
+      return data.data;
+    } else {
+      throw new Error('error');
+    }
+  } catch (error) {
+    const err = error as AxiosError;
+    return rejectWithValue(err.message);
+  }
+});
 
 export const cryptoSlice = createSlice({
   name: 'cryptos',
@@ -20,6 +46,24 @@ export const cryptoSlice = createSlice({
     addPortfolio: (state, action: PayloadAction<TCryptoInfo>) => {
       state.portfolio = [...state.portfolio, action.payload];
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(
+      fetchAllCryptos.fulfilled,
+      (state, action: PayloadAction<TCryptoInfo[]>) => {
+        state.loading = false;
+        state.cryptos = action.payload;
+      },
+    );
+    builder.addCase(fetchAllCryptos.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(
+      fetchAllCryptos.rejected,
+      (state, action: PayloadAction<string | unknown>) => {
+        state.loading = false;
+      },
+    );
   },
 });
 

a04ff6fcede26082371d5fc09426d99244e55b2a
Егор Матяс <play.filin@gmail.com>
2023-03-17 16:22:44 +0300

    feat: added info about chart on page

--- src/components/UI/CoinChart.tsx
+++ src/components/UI/CoinChart.tsx
@@ -15,20 +15,66 @@ import { Line } from 'react-chartjs-2';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { fetchCryptoChartData } from '../../redux/cryptoSlice';
 import { useLocation } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { TCryptoChart } from '../../types/types';
 import { CoinAddButton } from './CoinInfoUI';
 
 const ChartWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 55px 0px 0px 0px;
+  padding: 20px 0px 0px 0px;
 `;
 
 const ChartButtons = styled.div`
   display: flex;
   justify-content: space-around;
-  padding: 10px 50px 0px 50px;
+  padding: 30px 50px 0px 50px;
+`;
+
+const ChartInfo = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  color: black;
+  justify-content: space-around;
+  padding: 0 10px 20px 10px;
+  @media (max-width: 350px) {
+    justify-content: flex-start;
+  }
+`;
+
+const ChartInfoDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 600px) {
+    font-size: 13px;
+  }
+  @media (max-width: 445px) {
+    min-width: 160px;
+  }
+  p {
+    margin-bottom: 10px;
+    font-weight: 500;
+  }
+  span {
+    display: flex;
+    margin-bottom: 10px;
+    color: grey;
+    p:not(.last-p) {
+      margin-left: 10px;
+      margin-bottom: 0px;
+      color: black;
+    }
+  }
+`;
+
+const CryptoChange = styled.p`
+  ${({ color }) => {
+    return css`
+      margin-left: 10px;
+      margin-bottom: 0px;
+      color: ${Number(color) > 0 ? 'rgb(24, 198, 131)' : 'red'};
+    `;
+  }}
 `;
 
 ChartJS.register(
@@ -42,10 +88,14 @@ ChartJS.register(
   Legend,
 );
 
-const CoinChart = () => {
+const CoinChart: React.FC<{ nameCoin: string; symbolCoin: string }> = ({
+  nameCoin,
+  symbolCoin,
+}) => {
   const [dateFilter, setDateFilter] = useState<string>('day');
   const dispatch = useAppDispatch();
   const chartData = useAppSelector((state) => state.cryptos.cryptoChart);
+  const chartInfo = useAppSelector((state) => state.cryptos.cryptoChartInfo);
   const name = useLocation().pathname.split('/').pop();
 
   const chartDataLoading = async (dateFilter: string) => {
@@ -114,6 +164,33 @@ const CoinChart = () => {
 
   return (
     <ChartWrapper>
+      <ChartInfo>
+        <ChartInfoDiv>
+          <p>
+            {nameCoin}({symbolCoin})
+          </p>
+          <span>{new Date().toLocaleDateString()}</span>
+        </ChartInfoDiv>
+        <ChartInfoDiv>
+          <span>
+            HIGH: <p>${chartInfo.high}</p>
+          </span>
+          <span>
+            LOW: <p>${chartInfo.low}</p>
+          </span>
+        </ChartInfoDiv>
+        <ChartInfoDiv>
+          <span>
+            AVERAGE: <p>${chartInfo.average}</p>
+          </span>
+          <span>
+            CHANGE:{' '}
+            <CryptoChange className='last-p' color={chartInfo.change}>
+              {chartInfo.change}%
+            </CryptoChange>
+          </span>
+        </ChartInfoDiv>
+      </ChartInfo>
       <Line options={options} data={data} />
       <ChartButtons>
         <CoinAddButton onClick={() => setDateFilter('day')}>Day</CoinAddButton>
